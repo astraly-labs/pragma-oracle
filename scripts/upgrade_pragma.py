ee747741979cb9ae89827113d423ff6531af4389
0xevolve <Artevolve@yahoo.com>
2023-11-03 22:24:48 +0100

    ⬆️ testnet upgrade

--- scripts/upgrade_pragma.py
+++ scripts/upgrade_pragma.py
@@ -6,7 +6,6 @@ from math import ceil, log
 from scripts.utils.constants import (
     COMPILED_CONTRACTS,
     NETWORK,
-
 )
 from scripts.utils.starknet import (
     dump_declarations,
@@ -17,27 +16,37 @@ from scripts.utils.starknet import (
     invoke,
     deploy_v2,
     declare_v2,
-    call
+    call,
 )
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-admin_address = 0x02356B628D108863BAF8644C945D97BAD70190AF5957031F4852D00D0F690A77
 
 
 # %% Main
 async def main():
     # %% Declarations
     chain_id = NETWORK["chain_id"]
-    logger.info(
-        f"ℹ️  Connected to CHAIN_ID { chain_id }"
-    )
-    new_implementation_hash =await declare_v2('pragma_Oracle')
-    tx_hash = await invoke(
-                "pragma_Oracle", "upgrade", [new_implementation_hash]
-            )
-    logger.info(f"Upgaded the oracle contract with tx {hex(tx_hash)}")
+    logger.info(f"ℹ️  Connected to CHAIN_ID { chain_id }")
+
+    account = await get_starknet_account()
+    logger.info(f"ℹ️  Using account {hex(account.address)} as upgrader")
+
+    declarations = get_declarations()
+    cur_class_hash = declarations["pragma_Oracle"]
+    logger.info(f"ℹ️  Current implementation hash: {cur_class_hash}")
+
+    new_implementation_hash = await declare_v2("pragma_Oracle")
+    logger.info(f"ℹ️  New implementation hash: {new_implementation_hash}")
+
+    tx_hash = await invoke("pragma_Oracle", "upgrade", [new_implementation_hash])
+
+    declarations["pragma_Oracle"] = new_implementation_hash
+    dump_declarations(declarations)
+
+    logger.info(f"Uprgaded the oracle contract with tx {hex(tx_hash)}")
+
     logger.info("✅ Upgrade Completed")
 
 
